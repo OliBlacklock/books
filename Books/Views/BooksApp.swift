a92d88c78085961bf38c1be62c0079249e989bf8
oli <oli@googlemail.com>
2022-09-14 20:51:38 +0100

    Add json data

---
+++ Books/Views/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Oliver on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
